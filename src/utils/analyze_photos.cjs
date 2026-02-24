5fa061053d559ac33703b116277848f6ceda01b4
toyax <contact@toyaxo.ch>
2026-02-24 22:05:11 +0100

    - Added gallery (/photos)

--- src/utils/analyze_photos.cjs
+++ src/utils/analyze_photos.cjs
@@ -3,16 +3,19 @@ const ExifReader = require('exifreader');
 
 let photos = [];
 
-fs.readdirSync("../assets/photos").forEach((file)=>{
-    const fRead = fs.readFileSync(`../assets/photos/${file}`);
+fs.readdirSync("./src/assets/photos").forEach((file)=>{
+    const fRead = fs.readFileSync(`./src/assets/photos/${file}`);
     const tags = ExifReader.load(fRead);
 
     const date = tags["DateTime"]?.description.replace(" ", ":").split(":")
+    const height = tags['Image Height']?.value;
+    const width = tags['Image Width']?.value;
+    const type = height >= width ? "portrait" : "landscape";
 
     photos.push({
         name: file,
-        height: tags['Image Height']?.description,
-        width: tags['Image Width']?.description,
+        height: height,
+        width: width,
         model: tags["Model"]?.description,
         date: `${date[0]}.${date[1]}.${date[2]}`,
         time: `${date[3]}h${date[4]}m`,
@@ -20,8 +23,9 @@ fs.readdirSync("../assets/photos").forEach((file)=>{
         fNumber: tags["FNumber"]?.description,
         ISO: tags["ISOSpeedRatings"]?.description,
         focalLength: tags["FocalLength"]?.description,
-        lens: tags["LensModel"]?.description
+        lens: tags["LensModel"]?.description,
+        rotation: type
     });
 });
 
-fs.writeFileSync('../assets/photos-info.json', JSON.stringify(photos, null, 2))
+fs.writeFileSync('./src/assets/photos-info.json', JSON.stringify(photos, null, 2))
